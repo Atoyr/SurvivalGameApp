341c938ba669f01198c55484160f09c6220e32ff
Atoyr <lessironglance@gmail.com>
2017-04-14 00:45:51 +0900

    ウィンドウの透過

--- Source/SurvivalGameApp.Main/Views/Shell.xaml.cs
+++ Source/SurvivalGameApp.Main/Views/Shell.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -19,6 +21,10 @@ namespace SurvivalGameApp.Main.Views
     /// </summary>
     public partial class Shell : Window
     {
+        const int GWL_STYLE = -16;
+        const int WS_SYSMENU = 0x00080000;
+
+
         public Shell()
         {
             InitializeComponent();
@@ -30,5 +36,20 @@ namespace SurvivalGameApp.Main.Views
             SetBinding(WidthProperty, "Width");
             SetBinding(HeightProperty, "Height");
         }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            IntPtr handle = (new WindowInteropHelper(this)).Handle;
+            var style = GetWindowLong(handle, GWL_STYLE);
+            style &= (~WS_SYSMENU);
+            SetWindowLong(handle, GWL_STYLE, style);
+        }
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
     }
 }
