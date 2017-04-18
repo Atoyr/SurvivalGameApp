a02aa969c942450eeeb766832f037c644b58a74c
r.uchiyama <r.uchiyama@sint.co.jp>
2017-04-18 16:54:02 +0900

    キャプションバーの実装

---
+++ Source/SurvivalGameApp.Main/Controls/SystemButtons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SurvivalGameApp.Main.Controls
+{
+    class SystemButtons : Control
+    {
+        static SystemButtons()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SystemButtons), new FrameworkPropertyMetadata(typeof(SystemButtons)));
+        }
+    }
+}
