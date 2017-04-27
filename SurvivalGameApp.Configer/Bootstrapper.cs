763f2b53a2eaa4465a74125046615dc8fe0f9a7b
r.uchiyama <r.uchiyama@sint.co.jp>
2017-04-27 14:24:45 +0900

    Configファイル設定用のモジュール作成

---
+++ SurvivalGameApp.Configer/Bootstrapper.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Prism.Modularity;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SurvivalGameApp.Configer
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<Shell>();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            var catalog = (ModuleCatalog)this.ModuleCatalog;
+            catalog.AddModule(typeof(MainModule));
+        }
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            ViewModelLocationProvider.SetDefaultViewModelFactory(t =>
+                this.Container.Resolve(t));
+        }
+
+        protected override void InitializeShell()
+        {
+            // Shellを表示する
+            ((Window)this.Shell).Show();
+        }
+
+        public new void Run()
+        {
+            base.Run();
+            ExecutedRun(nameof(MenuView));
+        }
+        public void Run(string source)
+        {
+            base.Run();
+            ExecutedRun(source);
+        }
+
+        private void ExecutedRun(string source)
+        {
+            Container.RegisterInstance((Window)Shell);
+            NavigationHelper.GoNavigate(source);
+        }
+
+    }
+}
---
+++ SurvivalGameApp.Configer/Module.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurvivalGameApp.Configer
+{
+    class Module : IModule
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        [Dependency]
+        public IRegionManager RegionManager { get; set; }
+
+        public void Initialize()
+        {
+        }
+    }
+}
